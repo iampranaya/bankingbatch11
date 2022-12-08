acfd4a6c453ba91a8e28d67cdfb7967cd81791a0
PranayaSingh <iampranaya123@gmail.com>
2022-12-07 19:46:56 -0600

    statement homework

--- cubic-bank/banking-dao/src/main/java/com/rab3tech/customer/dao/repository/CustomerStatementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rab3tech.customer.dao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rab3tech.dao.entity.Statement;
-
-public interface CustomerStatementRepository extends JpaRepository<Statement, Integer>{
-
-}
--- cubic-bank/banking-service/src/main/java/com/rab3tech/customer/service/impl/CustomerStatementServiceImpl.java
+++ cubic-bank/banking-service/src/main/java/com/rab3tech/customer/service/impl/CustomerStatementServiceImpl.java
@@ -1,26 +1,59 @@
 package com.rab3tech.customer.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.rab3tech.customer.dao.repository.CustomerStatementRepository;
+import com.rab3tech.customer.dao.repository.CustomerAccountInfoRepository;
+
+import com.rab3tech.customer.dao.repository.TransactionRepository;
 import com.rab3tech.customer.service.CustomerStatementService;
+import com.rab3tech.dao.entity.CustomerAccountInfo;
+import com.rab3tech.dao.entity.Transaction;
 import com.rab3tech.vo.StatementVO;
 
 @Service
 @Transactional
 public class CustomerStatementServiceImpl implements CustomerStatementService {
-	
+
 	@Autowired
-	 CustomerStatementRepository CustomerStatementRepository;
+	CustomerAccountInfoRepository customerAccountInfoRepository;
+
+	@Autowired
+	TransactionRepository transactionRepository;
 
 	@Override
 	public List<StatementVO> showCustomerStatement(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		List<StatementVO> statementVO = new ArrayList<StatementVO>();
+
+		Optional<CustomerAccountInfo> customerAccountInformation = customerAccountInfoRepository.findByCustomerId(username);
+		// CustomerAccountInfo customerInfo = customerAccountInformation.get();
+		
+		// vo.setAccountNumber(customerAccountInformation.get().getAccountNumber());
+		if (customerAccountInformation.isPresent()) {
+			List<Transaction> transaction = transactionRepository.findByCustomerIdOrPayeeAccountNo(username,customerAccountInformation.get().getAccountNumber());
+
+			for (Transaction trans : transaction) {
+				StatementVO vo = new StatementVO();				
+				BeanUtils.copyProperties(trans, vo);				
+				if (trans.getCustomerId().equals(username)) {
+					vo.setAccountType("Debit");
+					vo.setAccountNumber(trans.getPayeeAccountNo());
+					
+					} else {
+					vo.setAccountType("CREDIT");
+					vo.setAccountNumber(customerAccountInformation.get().getAccountNumber());
+					}
+				statementVO.add(vo);
+			}
+
+		}
+		return statementVO;
 	}
 
 }
